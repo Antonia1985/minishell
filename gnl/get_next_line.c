9f59d3ed625ece77852b915cfe944a3b9fc64a6d
Antonia <33874536+Antonia1985@users.noreply.github.com>
2025-05-27 11:43:11 +0200

    Create get_next_line.c

---
+++ gnl/get_next_line.c
@@ -0,0 +1,95 @@
+#include "get_next_line.h"
+
+char	*ft_read_to_stash(int fd, char *stash)
+{
+	char	*buf;
+	int		bytes;
+
+	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(stash, '\n') && bytes != 0)
+	{
+		bytes = read(fd, buf, BUFFER_SIZE);
+		if (bytes == -1)
+		{
+			free(buf);
+			free(stash);
+			return (NULL);
+		}
+		buf[bytes] = '\0';
+		stash = ft_strjoin1(stash, buf);
+	}
+	free(buf);
+	return (stash);
+}
+
+char	*ft_get_line(char *stash)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!stash[i])
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	if (stash[i] == '\n')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_new_stash(char *stash)
+{
+	int		i;
+	int		j;
+	char	*new_stash;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+	{
+		free(stash);
+		return (NULL);
+	}
+	new_stash = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
+	if (!new_stash)
+		return (NULL);
+	i++;
+	j = 0;
+	while (stash[i])
+		new_stash[j++] = stash[i++];
+	new_stash[j] = '\0';
+	free(stash);
+	return (new_stash);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = ft_read_to_stash(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = ft_get_line(stash);
+	stash = ft_new_stash(stash);
+	return (line);
+}
